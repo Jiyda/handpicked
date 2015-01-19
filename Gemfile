64bd080fd710948818b4bfc253efbda5b5ca4f47
jailalawat <jailalawat@gmail.com>
2015-01-19 20:31:08 +0530

    bootstrap removed

--- Gemfile
+++ Gemfile
@@ -45,7 +45,6 @@ gem "paperclip"
 gem 'devise'
 gem "less-rails"
 gem "font-awesome-rails"
-gem 'twitter-bootstrap-rails'
 gem 'acts_as_votable', '~> 0.10.0'
 gem 'ransack'
 gem 'omniauth-facebook'
---
+++ app/assets/javascripts/front.js
@@ -0,0 +1,28 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+
+
+
+$(function() {
+  var loading_posts;
+  loading_posts = false;
+  return $('a.load-more-posts').on('inview', function(e, visible) {
+    if (loading_posts || !visible) {
+      return;
+    }
+    loading_posts = true;
+    return $.getScript($(this).attr('href'), function() {
+      return loading_posts = false;
+    });
+  });
+});
